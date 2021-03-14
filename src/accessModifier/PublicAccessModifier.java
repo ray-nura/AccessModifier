5cd52588950a20a80e57de34ed3e83b7b6789d2d
ray-nura <raynura182@users.noreply.github.com>
2021-03-13 18:56:57 -0800

    Access Modifier

--- src/accessModifier/PublicAccessModifier.java
+++ src/accessModifier/PublicAccessModifier.java
@@ -3,7 +3,7 @@ package accessModifier;
 public class PublicAccessModifier {
 	public void display1() 
     { 
-        System.out.println("This is Public Access Specifiers"); 
+        System.out.println("This is Public Access Specifiers called from Pack2 package"); 
     } 
 
 
